115c224cae97a026c0e1404353cabfb9caddb42c
AswinPuthukaatil <aswin100396@gmail.com>
2025-05-01 23:35:16 +0530

    moved the configuration logics to config from main for seperation of concerns

--- config/config.go
+++ config/config.go
@@ -1,1 +1,18 @@
 package config
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	Port string
+)
+
+func InitConfig() {
+	Port = os.Getenv("PORT")
+	if Port == "" {
+		Port = "8080" 
+		log.Println("PORT not set in .env, using default 8080")
+	}
+}
--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"log"
+	"github.com/joho/godotenv"
+	"github.com/Prototype-1/freelanceX_apigateway_service/config"
 	"github.com/Prototype-1/freelanceX_apigateway_service/internal/client"
-	redis "github.com/Prototype-1/freelanceX_apigateway_service/pkg"
 	"github.com/Prototype-1/freelanceX_apigateway_service/internal/router"
+	redis "github.com/Prototype-1/freelanceX_apigateway_service/pkg"
 )
 
 func main() {
-	redis.InitRedis()
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found, using environment variables")
+	}
+
+	config.InitConfig()
+
+	redis.InitRedis()
 	client.InitUserServiceClients()
 
 	r := router.SetupRouter()
 
-	if err := r.Run(":8080"); err != nil {
+	if err := r.Run(":" + config.Port); err != nil {
 		log.Fatalf("Failed to run server: %v", err)
 	}
 }
